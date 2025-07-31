67ac7fd7889e779b2cf6dee6f83e5afad35c3dd0
blackteabox <blackteabox.dev@gmail.com>
2025-07-31 15:01:11 +0900

    Initial Commit

---
+++ AAE8/AAE8App.swift
@@ -0,0 +1,17 @@
+//
+//  AAE8App.swift
+//  AAE8
+//
+//  Created by 이지선 on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct AAE8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AAE8/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AAE8
+//
+//  Created by 이지선 on 7/31/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
